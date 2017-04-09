9cd74f72ea1a26fc1482764edb417ceaa807fb14
Filip Stanis <sfilipx@gmail.com>
2017-04-09 22:56:16 +0100

    refactor to add all commands to client, added response parsing

--- frame_test.go
+++ frame_test.go
@@ -6,9 +6,9 @@ import (
 )
 
 func TestHandshake(t *testing.T) {
-	frame := &Frame{}
-	frame.CommandType = CMD_HANDSHAKE
-	data := frame.Build()
+	frm := &frame{}
+	frm.CommandType = commandTypeHandshake
+	data := frm.Build()
 
 	want := []byte{0xA5, 0x00, 0x09, 0x00, 0xCC, 0x33, 0xC3, 0x3C, 0xAC}
 	if !reflect.DeepEqual(data, want) {
@@ -17,13 +17,13 @@ func TestHandshake(t *testing.T) {
 }
 
 func TestLine(t *testing.T) {
-	frame := &Frame{}
-	frame.CommandType = CMD_DRAW_LINE
-	frame.DataAddShort(20)
-	frame.DataAddShort(45)
-	frame.DataAddShort(80)
-	frame.DataAddShort(70)
-	data := frame.Build()
+	frm := &frame{}
+	frm.CommandType = commandTypeDrawLine
+	frm.DataAddShort(20)
+	frm.DataAddShort(45)
+	frm.DataAddShort(80)
+	frm.DataAddShort(70)
+	data := frm.Build()
 
 	want := []byte{0xA5, 0x00, 0x11, 0x22, 0x00, 0x14, 0x00, 0x2D, 0x00, 0x50, 0x00, 0x46, 0xCC, 0x33, 0xC3, 0x3C, 0xB9}
 	if !reflect.DeepEqual(data, want) {
@@ -32,12 +32,22 @@ func TestLine(t *testing.T) {
 }
 
 func TestText(t *testing.T) {
-	frame := &Frame{}
-	frame.CommandType = CMD_DRAW_STRING
-	frame.DataAddShort(20)
-	frame.DataAddShort(45)
-	frame.DataAddString("test")
-	data := frame.Build()
+	frm := &frame{}
+	frm.CommandType = commandTypeDisplayText
+	frm.DataAddShort(20)
+	frm.DataAddShort(45)
+	frm.DataAddString("test")
+	data := frm.Build()
+
+	want := []byte{0xA5, 0x00, 0x12, 0x30, 0x00, 0x14, 0x00, 0x2D, 0x74, 0x65, 0x73, 0x74, 0x00, 0xCC, 0x33, 0xC3, 0x3C, 0xA8}
+	if !reflect.DeepEqual(data, want) {
+		t.Fatalf("Got %X, want %X", data, want)
+	}
+}
+
+func TestFrame(t *testing.T) {
+	frm := newFrame(commandTypeDisplayText, uint16(20), uint16(45), "test")
+	data := frm.Build()
 
 	want := []byte{0xA5, 0x00, 0x12, 0x30, 0x00, 0x14, 0x00, 0x2D, 0x74, 0x65, 0x73, 0x74, 0x00, 0xCC, 0x33, 0xC3, 0x3C, 0xA8}
 	if !reflect.DeepEqual(data, want) {
