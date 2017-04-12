a55631f1d0464f8a35935fd7175164b4af4e61df
Filip Stanis <sfilipx@gmail.com>
2017-04-12 22:52:59 +0100

    added interface

--- client.go
+++ client.go
@@ -7,6 +7,38 @@ const (
 	maxResponseSize = 32
 )
 
+type Device interface {
+	Handshake() error
+	SetBaudRate(rate int) error
+	GetBaudRate() (int, error)
+	IsStorageSDCard() (bool, error)
+	SetStorageType(useMicroSD bool) error
+	Sleep() error
+	Update() error
+	IsRotated() (bool, error)
+	SetRotation(rotated bool) error
+	LoadFonts() error
+	LoadImages() error
+	SetColor(foreground Color, background Color) error
+	GetColor() (Color, Color, error)
+	GetEnglishFontSize() (FontSize, error)
+	GetChineseFontSize() (FontSize, error)
+	SetEnglishFontSize(size FontSize) error
+	SetChineseFontSize(size FontSize) error
+	FillPixel(x uint16, y uint16) error
+	DrawLine(x1 uint16, y1 uint16, x2 uint16, y2 uint16) error
+	FillRect(x1 uint16, y1 uint16, x2 uint16, y2 uint16) error
+	DrawRect(x1 uint16, y1 uint16, x2 uint16, y2 uint16) error
+	DrawCircle(x uint16, y uint16, r uint16) error
+	FillCircle(x uint16, y uint16, r uint16) error
+	DrawTriangle(x1 uint16, y1 uint16, x2 uint16, y2 uint16, x3 uint16, y3 uint16) error
+	FillTriangle(x1 uint16, y1 uint16, x2 uint16, y2 uint16, x3 uint16, y3 uint16) error
+	Clear() error
+	DisplayText(x uint16, y uint16, text string) error
+	DisplayImage(x uint16, y uint16, file string) error
+	Close() error
+}
+
 type Client struct {
 	port *serial.Port
 }
